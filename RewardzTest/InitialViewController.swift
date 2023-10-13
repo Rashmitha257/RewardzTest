7b46725ade112bc5477d6dbb2b60547f84b9d1cf
Rashmitha <rashmitha.rangaswamy@softsuave.com>
2023-10-13 08:02:19 +0530

    first commit

---
+++ RewardzTest/InitialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RewardzTest
+//
+//  Created by Softsuave on 13/10/2023.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
